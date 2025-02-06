de6b948bc8e55c3313ce4c64393976f418a0bbc2
Mike Chan <mc0281@gmail.com>
2025-02-06 11:24:00 -0800

    Fixed 'units' errors in Constants.h
    Added missing header files.
    
    Change-Id: I3e31e682c77976236d4259d6f9ba2e1bdc999ad8
    Signed-off-by: Mike Chan <mc0281@gmail.com>

--- y2025/src/main/include/Constants.h
+++ y2025/src/main/include/Constants.h
@@ -1,24 +1,29 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
-#include "units/base.h"
+#include "generated/TunerConstants.h"
+#include "units/angular_velocity.h"
+#include "units/velocity.h"
+
 namespace DriveConstants {
-    // Initial Tuner constants
-    constexpr units::meters_per_second_t kMaxSpeed = TunerConstants::kSpeedAt12Volts; // kSpeedAt12Volts desired top speed
-    constexpr units::radians_per_second_t kMaxAngularRate = 0.75_tps; // 3/4 of a rotation per second max angular velocity
+// Initial Tuner constants
+constexpr units::meters_per_second_t kMaxSpeed =
+    TunerConstants::kSpeedAt12Volts;  // kSpeedAt12Volts desired top speed
+constexpr units::radians_per_second_t kMaxAngularRate =
+    0.75_tps;  // 3/4 of a rotation per second max angular velocity
 
-    // Controller dampening constants
-    const float kControllerVelocityCurveExponent = 2.0;
-    const float kControllerRotationCurveExponent = 2.0;
-    const double kControllerDeadbandPercentage = 0.02;
+// Controller dampening constants
+const float kControllerVelocityCurveExponent = 2.0;
+const float kControllerRotationCurveExponent = 2.0;
+const double kControllerDeadbandPercentage = 0.02;
 
-    // Softens the behavior of sudden controller input https://docs.wpilib.org/en/stable/docs/software/advanced-controls/filters/slew-rate-limiter.html
-    constexpr auto kSlewTranslateLimit = 0.6 * kMaxSpeed / 1_s;
-    constexpr auto kSlewRotateLimit = 1.0 * kMaxAngularRate / 1_s;
+// Softens the behavior of sudden controller input
+// https://docs.wpilib.org/en/stable/docs/software/advanced-controls/filters/slew-rate-limiter.html
+constexpr auto kSlewTranslateLimit = 0.6 * kMaxSpeed / 1_s;
+constexpr auto kSlewRotateLimit = 1.0 * kMaxAngularRate / 1_s;
 
-    // Button board
-    const int kClimbButton = 4;
-    const int kUnclimbButton = 2;
-}
+// Button board
+const int kClimbButton = 4;
+const int kUnclimbButton = 2;
+}  // namespace DriveConstants
 
-
-#endif // CONSTANTS_H
+#endif  // CONSTANTS_H
