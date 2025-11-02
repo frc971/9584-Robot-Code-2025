fde65b6b70e3fd295cef38b730649c0b6d3856a3
Bang Xiao <bangxiao0927@gmail.com>
2025-11-02 12:42:47 -0800

    Update TunerConstants.h
    Signed-off-by: Bang Xiao <bangxiao0927@gmail.com>

--- y2025/src/main/include/generated/TunerConstants.h
+++ y2025/src/main/include/generated/TunerConstants.h
@@ -101,7 +101,7 @@ class TunerConstants {
   static constexpr bool kInvertLeftSide = false;
   static constexpr bool kInvertRightSide = true;
 
-  static constexpr int kPigeonId = 1;
+  static constexpr int kPigeonId = 15;
 
   // These are only used for simulation
   static constexpr units::kilogram_square_meter_t kSteerInertia = 0.01_kg_sq_m;
